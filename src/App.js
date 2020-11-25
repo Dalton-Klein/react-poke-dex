82194f295465e8e27b3e26499938705b459586b3
Dalton Klein <deklein@live.com>
2020-11-25 09:27:07 -0600

    First Update After Experimenting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList'
+import Pagination from './Pagination'
+import axios from 'axios'
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [previousPageUrl, setPreviousPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+  const [banana, setBanana] = useState(5)
+
+  function incrBanana () {
+    console.log('How Many: ' , banana);
+    setBanana(prevBanana => prevBanana + 1)
+  }
+
+  //Only fetch list of pokemon once
+  useEffect(() => {
+    setLoading(true)
+    let cancel;
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPreviousPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.url))
+    })
+
+    return () => cancel()
+
+  }, [currentPageUrl])
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPreviousPage() {
+    setCurrentPageUrl(previousPageUrl)
+  }
+
+  if (loading) return "Loading..."
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>  
+    <PokemonList pokemon = {pokemon} />
+    <Pagination
+      gotoNextPage={nextPageUrl ? gotoNextPage : null}
+      gotoPreviousPage={previousPageUrl ? gotoPreviousPage : null}
+    />
+    <button onClick={incrBanana}> Increment Banana </button>
+    <span>{banana}</span>
+  </>
   );
 }
 
